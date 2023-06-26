a9b16f29b29e96f885a584d9a0bb7fde16eae53c
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-26 19:57:39 -0400

    Fixing some of the docstrings and adding the delete and get method using a state's id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creating a new view for State objects """
+"""Creating a new view for State objects."""
 
 from api.v1.views import app_views
 from flask import abort, jsonify
@@ -9,6 +9,27 @@ from models.state import State
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """get state information for all states"""
+    """Get state information for all states."""
     states = [state.to_dict() for state in storage.all("State").values()]
     return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Select a state by it's id."""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """Delete a state by using it's id."""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
