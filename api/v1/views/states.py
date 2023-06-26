377bfd546f35569826dca5fb439b5e8978705cc6
josemls <jose_9513@hotmail.com>
2023-06-26 12:15:34 -0400

    fixing get_states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,23 +7,10 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """ focus on all the state object """
-
-    if request.method == 'POST':
-        data = request.get_json()
-        if not data:
-            return Response("Not a JSON", 400)
-        if 'name' not in data:
-            return Response("Missing name", 400)
-        state = State(name=data.get('name'))
-        state.save()
-        return jsonify(state.to_dict()), 201
-
-    all_states = storage.all('State')
+    """get state information for all states"""
     states = []
-
-    for state in all_states.values():
+    for state in storage.all("State").values():
         states.append(state.to_dict())
     return jsonify(states)
