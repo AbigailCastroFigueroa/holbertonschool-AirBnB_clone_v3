ee7eb67dd85288094e65762dbc85d2468fb7db2e
josemls <jose_9513@hotmail.com>
2023-06-27 12:00:42 -0400

    fixing put_state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -33,7 +33,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
-    
+
 
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
