87a3273f00923a586d525831447b7db08f282351
josemls <jose_9513@hotmail.com>
2023-06-28 13:44:22 -0400

    fixing sintax error

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -19,7 +19,7 @@ def get_cities(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/api/v1/cities/<city_id>', methods=['GET'],
+@app_views.route('/cities/<city_id>', methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id):
     """get city information for specified city"""
