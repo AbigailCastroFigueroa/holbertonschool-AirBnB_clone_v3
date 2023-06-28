469b57b055bd0038698676ee9385f1ae196a46da
josemls <jose_9513@hotmail.com>
2023-06-28 12:33:28 -0400

    fixing route in cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
+@app_views.route('/api/v1/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def get_cities(state_id):
     """get city information for all cities in a specified state"""
@@ -21,7 +21,7 @@ def get_cities(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'],
+@app_views.route('/api/v1/cities/<city_id>', methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id):
     """get city information for specified city"""
