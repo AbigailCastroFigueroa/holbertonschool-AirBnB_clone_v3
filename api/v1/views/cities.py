6e48ae7f5d7b18d3b3a2f88b9fa942a2f5f59917
josemls <jose_9513@hotmail.com>
2023-06-28 12:27:57 -0400

    adding cities.py and updating __init__.py to import new file

---
+++ api/v1/views/cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""cities attribute"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id):
+    """get city information for all cities in a specified state"""
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_city(city_id):
+    """get city information for specified city"""
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    return jsonify(city.to_dict())
