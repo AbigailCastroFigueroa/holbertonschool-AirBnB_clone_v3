e877862db8d633da1009ac20effa3f18e8977a0c
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-28 20:18:00 -0400

    Adding the storage.new method to the POST method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,7 +7,7 @@ from models import storage
 from models.amenity import Amenity
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieve a list of all the amenities."""
     amenities_list = []
@@ -16,7 +16,7 @@ def get_amenities():
     return jsonify(amenities_list)
 
 
-@app_views.route('amenities/<amenity_id>', methods=['GET'],
+@app_views.route('amenities/<amenity_id>/', methods=['GET'],
                  strict_slashes=False)
 def amenity_by_id(amenity_id):
     """Retrieve an amenity by its id."""
@@ -26,7 +26,7 @@ def amenity_by_id(amenity_id):
     return jsonify(specific_amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+@app_views.route('/amenities/<amenity_id>/', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
     """Delete selected amenity."""
@@ -38,28 +38,30 @@ def delete_amenity(amenity_id):
     return jsonify({}), 200
 
 
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+@app_views.route('/amenities/', methods=['POST'], strict_slashes=False)
 def post_amenity():
     """Create a new Amenity object."""
-    if not request.is_json():
+    data = request.get_json()
+    if not data:
         return make_response(jsonify(error="Not a JSON"), 400)
-    if 'name' not in request.get_json():
+    if 'name' not in data:
         return make_response(jsonify(error="Missing name"), 400)
-    new_amenity = Amenity(**request.get_json())
+    new_amenity = Amenity(**data)
+    storage.new(new_amenity)
     storage.save()
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>')
+@app_views.route('/amenities/<amenity_id>/')
 def amenities_update(amenity_id):
     """Update amenities attributes."""
     selected_amenity = storage.get(Amenity, amenity_id)
     if amenity_id not in selected_amenity:
         abort(404)
-    if not request.is_json():
+    data = request.get_json()
+    if not data:
         return make_response(jsonify(error="Not a JSON"), 400)
-    content = request.get_json()
-    for key, value in content.items():
+    for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(selected_amenity, key, value)
     storage.save()
